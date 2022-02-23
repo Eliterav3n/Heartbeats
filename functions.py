c191b1badd04f75a4860d5fde8f8241beb7367d5
Gary Khodayari <gkhodayari@escautomation.com>
2022-02-23 10:45:18 -0800

    new sending protocol

--- functions.py
+++ functions.py
@@ -1,60 +1,38 @@
 #!/usr/bin/python3
 
-import sys, time, socket
-from threading import Thread
+import pcapy
+from scapy.layers.inet import TCP, IP
+from scapy.sendrecv import sniff
 
-soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# a_filter = "tcp[13] & 8!=0" # Captures TCP-PSH packets.
+a_filter = "tcp"
+devs = pcapy.findalldevs() # available devices
+print(devs)
 
-
-def sending(host, port, data):
-    """
-    this function sends messages to a specified port and host
-    host: ip address to send the message to
-    port: port to send the message to
-    """
-    # Create a TCP socket
-    data = bytes(data)
-    soc.connect((host, port))
-    print(f'Connection to {host} stablished')
-    soc.sendall()
-
-
-def listening(port, bitsize):
-    """
-    this funtion listens to upcoming requests on a identified port
-    port: listening port
-    """
-    # Create a TCP/IP socket
-    soc.bind(("127.0.0.1", port))
-    # Set up to listen for incoming connections.
-    soc.listen()
-    print('Listing ...')
-    # Accept an incoming connection
-    conn, addr = soc.accept()
-    # Read and write the data
-    with conn:
-        print(f'Connected by {addr}')
-        while True:
-            data = conn.recv(bitsize)
-            if not data:
-                break
-            whattodo(data=data)
+def prnt_pckt(packet):
+    # IP WRAP
+    dst_ip = packet[IP].src
+    src_ip = packet[IP].dst
+    ip_ver = packet[IP].version
+    ip_proto = packet[IP].proto
+    # TCP WRAP
+    tcp_src_p = packet[TCP].sport
+    tcp_dst_p = packet[TCP].dport
+    try:
+        tcp_data = packet[TCP].load
+    except:
+        tcp_data = "empty packet"
+    
+    print('''
+    src ip : {}
+    dst ip : {}
+    ip ver : {}
+    ip proto : {}
+    src port : {}
+    dest port : {}
+    data : {}
+    '''.format(dst_ip, src_ip, ip_ver, tcp_src_p, ip_proto, tcp_dst_p, tcp_data))
 
 
-def whattodo(data):
-    """
-    this function decides what to do with the data in the packets
-    """
-    if data == "HELLO":
-        return "HELLO_THERE"
-    elif data == "BYE":
-        return "BYE_BYE"
-    elif data == "ANYONE_THERE":
-        return "IM_HERE"
-    else:
-        return "I_DONT_UNDERSTAND"
-
-# answer = soc.recv(bitsize)
-# # Provide results by received data from the server
-# print(f'Received {repr(answer)}')
-# print(f'Connection with {host} terminated')
+sniff(filter=a_filter, prn=prnt_pckt)
+# sniff(filter=a_filter, prn=lambda x: x.show())
--- test.py
+++ test.py
@@ -1,37 +1,17 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python
+from scapy.all import *
 
-import pcapy
-from scapy.layers.inet import TCP, IP
-from scapy.sendrecv import sniff
+# VARIABLES
+src = sys.argv[1]
+dst = sys.argv[2]
+sport = random.randint(1024,65535)
+dport = int(sys.argv[3])
 
-a_filter = "tcp[13] & 8!=0" # Captures TCP-PSH packets.
-devs = pcapy.findalldevs() # available devices
-print(devs)
+# SYN
+ip=IP(src=src,dst=dst)
+SYN=TCP(sport=sport,dport=dport,flags='S',seq=1000)
+SYNACK=sr1(ip/SYN)
 
-def prnt_pckt(packet):
-    # IP WRAP
-    dst_ip = packet[IP].src
-    src_ip = packet[IP].dst
-    ip_ver = packet[IP].version
-    ip_proto = packet[IP].proto
-    # TCP WRAP
-    tcp_src_p = packet[TCP].sport
-    tcp_dst_p = packet[TCP].dport
-    try:
-        tcp_data = packet[TCP].load
-    except:
-        tcp_data = "empty packet"
-    
-    print('''
-    src ip : {}
-    dst ip : {}
-    ip ver : {}
-    ip proto : {}
-    src port : {}
-    dest port : {}
-    data : {}
-    '''.format(dst_ip, src_ip, ip_ver, tcp_src_p, ip_proto, tcp_dst_p, tcp_data))
-
-
-sniff(filter=a_filter, prn=prnt_pckt)
-# sniff(filter=a_filter, prn=lambda x: x.show())
+# ACK
+ACK=TCP(sport=sport, dport=dport, flags='A', seq=SYNACK.ack, ack=SYNACK.seq + 1)
+send(ip/ACK)
